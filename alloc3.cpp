807dc6dce587f2487df4e05f9a4b4e48bf0b7314
Partha Sarma <131948502+ParthaSarma94@users.noreply.github.com>
2024-09-13 19:56:32 +0530

    BusFarecpp

--- alloc3.cpp
+++ alloc3.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int n, dist, fare;
+float rate = 1.6;
+string sp, dp;
+
+struct Places
+{
+    string pl;
+    float distance;
+};
+
+Places places[11] = {
+    {"jalukbari", 0},
+    {"adabari", 5},
+    {"maligaon", 10},
+    {"machkhowa", 14},
+    {"fancyBazaar", 15},
+    {"panbazar", 16},{"chandmari",20},{"commerceCollege",23},
+    {"jonali",25},{"ganeshguri",27},{"hatigaon",30}
+    };
+
 void clearS()
 {
 #ifdef _WIN32
@@ -10,21 +30,20 @@ void clearS()
 #endif
 }
 
-class fare
+int findDistance(string sp, string dp)
 {
-private:
-    const float rate = 1.6;
-    int n;
-    string sp, dp;
-    float dist;
+    int start = -1, end = -1;
+    for (int i = 0; i < 11; ++i)
+    {
+        if (places[i].pl == sp)
+            start = i;
+        if (places[i].pl == dp)
+            end = i;
+    }
+    return places[end].distance - places[start].distance;
+}
 
-public:
-    void getP();
-    int calcfare();
-    void display();
-} ep1;
-
-int fare::calcfare()
+int calcfare(int n, int dist, float rate)
 {
     if (n == 0)
     {
@@ -53,33 +72,30 @@ int fare::calcfare()
     }
 }
 
-void fare::getP()
-{
-    cout << "Enter start place: " << endl;
-    cin >> sp;
-    cout << "Enter destination: " << endl;
-    cin >> dp;
-    cout << "Distance:" << endl;
-    cin >> dist;
-    cout << "Number of person:" << endl;
-    cin >> n;
-}
 
-void fare::display()
+void display(string sp, string dp, int n, int dist)
 {
     cout << "----------BUS FARE---------" << endl;
     cout << "\n";
     cout << sp << " TO " << dp << endl;
     cout << "Distance : " << dist << " KM" << endl;
     cout << "FULL " << n << "X" << endl;
-    cout << "Fare::  Rs\033[1m" << calcfare() << "\033[0m";
+    cout << "Fare::  Rs\033[1m" << calcfare(n, dist, rate) << "\033[0m";
 }
 
 int main()
 {
+    cout << "Enter start place: " << endl;
+    cin >> sp;
+    cout << "Enter destination: " << endl;
+    cin >> dp;
+    dist = findDistance(sp, dp);
+    cout << "Number of person:" << endl;
+    cin >> n;
+
     clearS();
-    ep1.getP();
-    clearS();
-    ep1.display();
+    display(sp, dp, n, dist);
+
     return 0;
 }
+
